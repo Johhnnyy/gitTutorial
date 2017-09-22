5b37b616a01dd348904e12059f6d14ade7cd5975
Dhiraj <Dhiraj@192.168.0.100>
2017-09-22 14:59:50 +0530

    Added testMethod2

--- gitcheck/src/gitcheck/test1.java
+++ gitcheck/src/gitcheck/test1.java
@@ -1,15 +1,32 @@
 package gitcheck;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class test1 {
 
-	
-@Test
-public  void testMethod(){
-	System.out.println("test1 result");
-	System.out.println("first change");
-	System.out.println(System.getProperty("build"));
 
-}
+	@Test
+	public  void testMethod(){
+		int a = 2;
+		int b= 3;
+		System.out.println("test1 result");
+		System.out.println("first change");
+		System.out.println(System.getProperty("build"));
+
+
+	}
+
+	@Test
+	public  void testMethod2(){
+		int a = 2;
+		int b= 3;
+		System.out.println("test1 result");
+		System.out.println("first change");
+		System.out.println(System.getProperty("build"));
+		Assert.assertEquals(true, false);
+
+
+	}
+
 }
